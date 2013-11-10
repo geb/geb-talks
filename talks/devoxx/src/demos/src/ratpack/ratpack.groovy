a57386fd18aa5e7e6e392613281b6d2a2f36d99b
Luke Daley <ld@ldaley.com>
2013-11-10 18:22:32 +0000

    Clean up the demo code.

--- talks/devoxx/src/demos/src/ratpack/ratpack.groovy
+++ talks/devoxx/src/demos/src/ratpack/ratpack.groovy
@@ -1,25 +1,32 @@
-import org.ratpackframework.groovy.templating.TemplateRenderer
-
-import static org.ratpackframework.groovy.RatpackScript.ratpack
+import static ratpack.groovy.Groovy.*
 
 ratpack {
     handlers {
-        assets "public"
         get("login") {
-            def renderer = get(TemplateRenderer)
             def params = request.queryParams
             if (params.username) {
                 def model = [:]
                 model.username = params.username
                 model.password = params.password
                 model.remember = params.rememberme as boolean
-                model.success = (model.username == "user" && model.password == "password")
+                model.success = (model.username == "devoxx" && model.password == "devoxx")
 
-                renderer.render(model, "login-result.html")
+                render groovyTemplate(model, "login-result.html")
             } else {
-                renderer.render("login.html")
+                render groovyTemplate("login.html")
             }
         }
 
+        fileSystem("public") {
+            assets "", "index.html"
+            handler {
+                def toServe = file(request.path + ".html")
+                if (toServe.exists()) {
+                    render toServe
+                } else {
+                    next()
+                }
+            }
+        }
     }
 }
